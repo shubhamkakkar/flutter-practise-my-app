5d3b40a24746ba3b45f791e11840f359f567530a
Kakkar, shubham <shubham.kakkar98@gmail.com>
2021-09-26 23:45:06 +0530

    icon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Material App'),
+        title: const Text('Material App'),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
-      body: Center(child: Text('Hello', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold), )),
+      body: const Center(
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50,
+        )
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text('Click'),
+        onPressed: () => {},
+        child: const Text('click'),
         backgroundColor: Colors.red,
       ),
-    )
-  ));
+    );
+  }
+}
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: Home(),
+    ),
+  );
 }
